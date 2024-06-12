6d48a5cebe93b181b5dc11fecac50caadd49e1dd
Ishaan Awasthi <136520517+ishaan-awasthi@users.noreply.github.com>
2024-06-12 12:54:48 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,125 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println();
+        System.out.println("WELCOME TO MADLIBS!");
+        System.out.println();
+        System.out.print("Enter a noun: ");
+        String input1 = input.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String input2 = input.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String input3 = input.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String input4 = input.nextLine();
+
+        System.out.print("Enter a plural noun: ");
+        String input5 = input.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String input6 = input.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String input7 = input.nextLine();
+
+        System.out.print("Enter an article of clothing: ");
+        String input8 = input.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String input9 = input.nextLine();
+
+        System.out.print("Enter a name of a store: ");
+        String input10 = input.nextLine();
+
+        System.out.print("Enter a noun: ");
+        String input11 = input.nextLine();
+
+        System.out.print("Enter a famous name: ");
+        String input12 = input.nextLine();
+
+        System.out.print("Enter a past tense verb: ");
+        String input13 = input.nextLine();
+
+        System.out.print("Enter a number: ");
+        int input14 = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String input15 = input.nextLine();
+
+        System.out.print("Enter a facial expression: ");
+        String input16 = input.nextLine();
+
+        System.out.print("Enter a past tense verb: ");
+        String input17 = input.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String input18 = input.nextLine();
+
+        System.out.print("Enter a past tense verb: ");
+        String input19 = input.nextLine();
+
+        System.out.print("Enter a name of a person in class: ");
+        String input20 = input.nextLine();
+
+        System.out.print("Enter a small number: ");
+        int input21 = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter a past tense verb: ");
+        String input22 = input.nextLine();
+
+        System.out.print("Enter a small number: ");
+        int input23 = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String input24 = input.nextLine();
+
+        System.out.print("Enter a large float with two decimals: ");
+        float input25 = input.nextFloat();
+        input.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String input26 = input.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String input27 = input.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String input28 = input.nextLine();
+
+        System.out.print("Enter an exclamation: ");
+        String input29 = input.nextLine();
+
+        System.out.print("Enter a present participle verb (running): ");
+        String input30 = input.nextLine();
+
+        System.out.println();
+        System.out.println("         MR. JAN AND THE QUEST FOR BOBA");
+        System.out.println("------------------------------------------------------");
+        System.out.println("Once upon a " + input1 + ", there lived a " + input2 + " CS teacher named Mr. Jan. " + input3 + " and " + input4 + ", Mr. Jan was loved by all his " + input5 + ". \n\nAnd although Mr. Jan loved them too, there was one thing he loved more: boba! One Tuesday afternoon, he was feeling thirsty for a " + input6 + " boba tea. He " + input7 + " put on his " + input8 + " and embarked on his quest to find the perfect tea."
+                + "\n\nSoon, he arrived at the " + input9 + " boba tea shop " + input10 + ". He opened the " + input11 + " and saw his student " + input12 + " behind the counter." + input12 + " turned and " + input13 + ". \"What can I do for you Mr. Jan?\""
+                + "\n\n\"" + input14 + " milk teas, extra boba please!\" Mr. Jan replied. \"Coming right up!\" said " + input12 + ", disappearing " + input15 + " into the back room."
+                + "\n\nAfter a few seconds, he returned with a " + input16 + ". \"Bad news, Mr. Jan. We're all out of boba pearls.\""
+                + "\n\nMr. Jan " + input17 + ". \"Alright, just the milk teas then. No boba.\" He sat down " + input18 + " and " + input19 + "."
+                + "\n\nTwo minutes later, " + input12 + " returned. \"Sorry, I think " + input20 + " drank all the milk.\""
+                + "\n\nMr. Jan couldn't believe it. \"Alright, just plain tea then.\""
+                + "\n\n\"You got it, Mr. Jan!\" came the reply. But just " + input21 + " seconds later, " + input12 + " came back."
+                + "\n\n\"I hate to say this, but we're all out of tea too.\""
+                + "\n\n\"No! I'm not leaving until I get something! Anything!\" Mr. Jan " + input22 + ". \"Go look in the back, take your time and don't come back until you have something I can buy!\""
+                + "\n\n" + input23 + " minutes later, " + input12 + " returned carrying something in hand. \"I found one boba pearl, I can put that in some water for you.”"
+                + "\n\nDesperate for any sort of boba, Mr. Jan agreed. “How much do I owe you?” he asked " + input24 + "."
+                + "\n\n" + "\"" + input25 + " dollars. " + (input25 + 0.0725*input25) + " with tax.\""
+                + "\n\n“What?!” Mr. Jan shouted " + input26 + ". “That’s " + input27 + "!”"
+                + "\n\n“Blame inflation, not me!” said " + input12 + " " + input28 + "."
+                + "\n\n“Fine, fine.” Mr. Jan said, reaching for his wallet. And he reached and reached, yet found nothing."
+                + "\n\n“" + input29 + "! I forgot my wallet!” Mr. Jan yelled. And so he returned home, boba-less, " + input30 + " every step of the way."
+);
+    }
+}
